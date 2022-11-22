d6d13b78cfc10fa621fdee4043d996268f126d9e
Gaurav Prabhu <gauravprabhu15@gmail.com>
2022-11-22 12:42:50 +0530

    Updated api calls

--- api.js
+++ api.js
@@ -2,6 +2,7 @@ const express = require("express");
 require('dotenv').config();
 const cors = require("cors");
 const axios = require("axios");
+const bodyParser = require("body-parser");
 const helmet = require("helmet");
 
 const app = express();
@@ -11,6 +12,7 @@ const AUTHENDPOINT = "https://login.salesforce.com/services/oauth2/token";
 
 app.use(cors());
 app.use(helmet());
+app.use(bodyParser.json());
 
 const queryParams = {
    grant_type: "password",
@@ -20,21 +22,45 @@ const queryParams = {
    password: process.env.RENDER_LOGIN_PASSWORD
 };
 
-let responseData = null;
+app.post("/createlead", (req, res) => {
 
-axios
-.post(AUTHENDPOINT, null, {
-    params: queryParams
-})
-.then(res => {
-    responseData = res.data || {};
-}).catch(e => {
-    console.log("Error", e)
+    let request = req.body;
+    let result = null;
+    axios
+    .post(AUTHENDPOINT, null, {
+        params: queryParams
+    })
+    .then(async res => {
+        if(res.status === 200){
+            let data = res.data;
+            let accessToken = data.access_token,
+            instanceURL = data.instance_url || "https://roswaltrealty.my.salesforce.com";
+            let sendData = {
+                req: {...request}
+            };
+            const FORMURL = `${instanceURL}/services/apexrest/CreateLead/`;
+
+            await axios
+            .post(FORMURL, sendData, {
+                headers: {
+                    'Authorization': `Bearer ${accessToken}`,
+                    'Content-Type':'application/json'
+                }
+            }).then(res => {
+                console.log('Res status', res.status);
+                if(res.status === 200){
+                    let data = res.data;
+                    result = data;
+                }
+            })
+        }
+    }).then(resp => {
+        res.send(result);
+    }).catch(e => {
+        console.log("Error", e)
+    });
+
 });
 
-app.get("/", (req, res) => {
-    res.json(responseData);
-    res.end();
-});
 
 app.listen(PORT, () => console.log(`Hello world app listening on port ${PORT}!`));
