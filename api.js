410c16bcb5a6141f0563d5a07e03950d9b9998af
Ferb <88365695+devops0019@users.noreply.github.com>
2024-01-18 12:17:44 +0000

    Updated

--- api.js
+++ api.js
@@ -1,5 +1,5 @@
 const express = require("express");
-require('dotenv').config();
+require("dotenv").config();
 const cors = require("cors");
 const axios = require("axios");
 const bodyParser = require("body-parser");
